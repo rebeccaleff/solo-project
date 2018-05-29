f2bfa8ce2b57e850505f7637eac26fca55f72165
Rebecca Leff <rebeccacleff@gmail.com>
2018-05-29 16:33:37 -0400

    Working React main component with Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hello');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="yes">
+              <div className="header">
+                Hello {this.props.name}
+              </div>
+            </div>    
+        );
+    }
+}
+
+ReactDOM.render(<App name='Joanna'/>, document.getElementById("index"));
+
+// add script to start server
