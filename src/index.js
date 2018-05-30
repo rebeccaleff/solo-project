7b1ff9093655bc54e11ab8a25092eb2405d78201
Rebecca Leff <rebeccacleff@gmail.com>
2018-05-29 21:58:12 -0400

    Button working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+        this.eventHandler = this.eventHandler.bind(this);
+    }
+
+    eventHandler() {
+      console.log('I was clicked');
+        // when called with a click, then fetch request to localhost at specified route
+        // on server side, listen to the specific route 
+    }
+
     render() {
         return (
             <div className="yes">
               <div className="header">
                 Hello {this.props.name}
               </div>
+              <button onClick={this.eventHandler}>Click</button>
             </div>    
         );
     }
 }
 
-ReactDOM.render(<App name='Joanna'/>, document.getElementById("index"));
+ReactDOM.render(<App name='Joanna'/> , document.getElementById("index"));
 
 // add script to start server
+
+
+
